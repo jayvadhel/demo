ee17839d0a9ff830822582454f4fa7605c9f1db0
jayvadhel <eparis@atlassian.com>
2024-05-07 13:11:55 +0530

    changes before merge

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,6 @@ bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
 
-<<<<<<< HEAD
 
 // hii
 // this is latest branch
-=======
-// this is main branch
->>>>>>> 472f399db91460375f0e50f87f2b9925dee3ba4d
